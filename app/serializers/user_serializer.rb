636c3a5a561c47c7874f66ff3e1604c74619a573
schmartmann <stefanhartmann@gmail.com>
2019-01-17 15:53:04 -0500

    serializer user attributes better

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,7 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :email
+  attributes :id, :email, :provider, :uid, :allow_password_change, :name, :nickname,
+    :image, :created_at, :updated_at, :uuid
+
 
   has_many :workouts
 end
